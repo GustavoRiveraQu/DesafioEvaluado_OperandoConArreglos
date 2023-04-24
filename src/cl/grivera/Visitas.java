9eeb4fe965ffe45470b276108ef5d2a7a1f3c71e
GustavoRiveraQu <129341493+GustavoRiveraQu@users.noreply.github.com>
2023-04-24 19:57:22 -0400

    Initial commit

---
+++ src/cl/grivera/Visitas.java
@@ -0,0 +1,27 @@
+package cl.grivera;
+
+import java.util.ArrayList;
+
+public class Visitas {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		promedio();
+	}
+
+	public static void promedio() {
+		ArrayList<Integer> lista = new ArrayList<Integer>();
+		lista.add(3001);
+		lista.add(1402);
+		lista.add(1304);
+		lista.add(1505);
+		int suma = 0 ;
+
+
+		for (Integer e : lista) { 
+
+			suma = suma + e;
+		}
+		System.out.println("Para la entrada anterior el resultado es " + suma/lista.size());
+	}
+}
